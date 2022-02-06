fc2270f5bbeed7a415e3cf85312f4af1efcae5ff
skamuju <63597158+skamuju@users.noreply.github.com>
2022-02-06 01:54:01 -0500

    Fix weather.py

--- weather.py
+++ weather.py
@@ -11,10 +11,12 @@ def check_conditions(zipcode):
     weather_call = requests.get(
         f"http://api.openweathermap.org/data/2.5/weather?zip={zipcode},us&appid=bd85bd1838a37384556ace68c693d987")
 
-    temp = weather_call.json()["main"]["temp"]
-    conditions = weather_call.json()["weather"]["id"]
+    # return [temp, conditions]
+    temp = dict(weather_call.json())["main"]["temp"]
+    cond = dict(weather_call.json())["weather"][0]["id"]
 
-    return [temp, conditions]
+    return temp, cond
 
 print(check_conditions(27519))
+print(check_conditions("27519"))
     
