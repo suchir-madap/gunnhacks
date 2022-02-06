d0801893e9c8812ad0f8317ff06aa7f8c50a8bd4
skamuju <63597158+skamuju@users.noreply.github.com>
2022-02-06 12:59:45 -0500

    create movement in main.py

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 
 
 
+from turtle import onkey, onkeypress
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.core.window import Window
 from weather import *
 
 
 
-class snake_game(Widget):
-    pass
+class SnakeGame(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.keyboard = Window.request_keyboard(self.on_key_closed, self)
+        self.keyboard.bind(on_key_up=self.on_key_up)
 
-class snake_app(App):
+        with self.canvas:
+            self.player = Rectangle(pos=(500, 500), size=(100, 100))
+    
+    def on_key_closed(self):
+        self.keyboard.unbind(on_key_up=self.on_key_up)
+        self.keyboard = None
+    
+    def on_key_up(self, keyboard, keycode):
+        k_pressed = keycode[-1]
+        xpos = self.player.pos[0]
+        ypos = self.player.pos[1]
+        print(keycode[-1])
+
+        if k_pressed == "w":
+            ypos += 50
+        elif k_pressed == "a":
+            xpos -= 50
+        elif k_pressed == "s":
+            ypos -= 50
+        elif k_pressed == "d":
+            xpos += 50
+        
+        self.player.pos = (xpos,ypos)
+
+class SnakeApp(App):
     def build(self):
-        return snake_game()
+        return SnakeGame()
 
 
 if __name__ == "__main__":
-    z_code = int(input())
-    print(check_conditions(z_code))
-    # snake_app().run()
+    # z_code = int(input())
+    # print(check_conditions(z_code))
+    SnakeApp().run()
